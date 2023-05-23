6fca81f286102141fa4bc419dee89f8a1df02758
bipon68 <bipon68@users.noreply.github.com>
2023-05-23 22:57:45 +0600

    #1 basic req and res and api testing

--- ecommrce-mern/src/server.js
+++ ecommrce-mern/src/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.get("/test", (req, res) => {
+    res.status(200).send({
+        message: 'Welcome to Jungle'
+    })
+})
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: 'Welcome to My Home'
+    })
+})
+
+app.get("/products", (req, res) => {
+    res.status(200).send({
+        message: 'Products are retured.'
+    })
+})
+
 app.listen(3001, () => {
     console.log(`Server running at localhost://3001`)
 })
